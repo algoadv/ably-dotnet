d87238fda5597e807c452fd1aafae9bae29d730c
Apostolis <apostolis@cpmstars.com>
2019-10-02 18:19:16 +0300

    Unity project.

---
+++ src/IO.Ably.Unity/Platform.cs
@@ -0,0 +1,20 @@
+﻿using IO.Ably.Transport;
+using System.Net.NetworkInformation;
+using IO.Ably.Realtime;
+
+namespace IO.Ably
+{
+    internal class Platform : IPlatform
+    {
+        public string PlatformId => "unity";
+
+        public ITransportFactory TransportFactory => null;
+
+        static Platform()
+        {
+            Connection.NotifyOperatingSystemNetworkState(NetworkState.Online);
+            // NetworkChange.NetworkAvailabilityChanged += (sender, eventArgs) =>
+                // Connection.NotifyOperatingSystemNetworkState(eventArgs.IsAvailable ? NetworkState.Online : NetworkState.Offline);
+        }
+    }
+}
